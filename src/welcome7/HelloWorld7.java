6c1170fefd97f189ae9557da7143889d6a4df3cc
Megha Mapalagama <meghamapalagama@meghas-mbp.home>
2021-03-08 02:02:30 -0500

    second commit

--- src/welcome7/HelloWorld7.java
+++ src/welcome7/HelloWorld7.java
@@ -5,6 +5,8 @@ public class HelloWorld7 {
 	public static void main(String[] args) {
 		
 		System.out.println("I love coding !.......");
+		
+		System.out.println("***********************");
 
 	}
 
